045bd106768b4c4802e0fe736347e96a7213aa33
sofiahubert <sofia.hubert.gonzalez@gmail.com>
2023-08-10 19:31:17 -0300

    pagina que funciona

--- js/validacion.js
+++ js/validacion.js
@@ -5,43 +5,21 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
-document.addEventListener('DOMContentLoaded', function () {
-    const regBtn = document.getElementById('regBtn');
-    regBtn.addEventListener('click', function () {
-      const password1 = document.getElementById('password1').value;
-      const password2 = document.getElementById('password2').value;
+
+regBtn.addEventListener('click', function () {
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const email = document.getElementById('email').value;
+    const password1 = document.getElementById('password1').value;
+    const password2 = document.getElementById('password2').value;
+    const terminos = document.getElementById('terminos').checked;
   
-      if (password1 !== password2) {
-        alert("Las contraseñas no coinciden. Por favor, ingrésalas nuevamente.");
-      }
-    });
-  
-    // Resto del código de validación
+    if (nombre === '' || apellido === '' || email === '' || password1 === '' || password2 === '' || !terminos) {
+      showAlertError();
+    } else if (password1 !== password2) {
+      showAlertError();
+    } else {
+      showAlertSuccess(); // Llama a la función showAlertSuccess() si todo está correcto
+    }
   });
-  function validarFormulario() {
-    var nombre = document.getElementById("nombre").value;
-    var apellido = document.getElementById("apellido").value;
-    var email = document.getElementById("email").value;
-    var password1 = document.getElementById("password1").value;
-    var password2 = document.getElementById("password2").value;
-    var terminos = document.getElementById("terminos").checked;
-  
-    if (nombre === "" || apellido === "" || email === "" || password1 === "" || password2 === "" || !terminos) {
-      showAlertError();
-      return false; // Evita que el formulario se envíe si algún campo está vacío o no se aceptaron términos
-    }
-  
-    if (password1.length < 6) {
-      showAlertError();
-      return false; // Evita que el formulario se envíe si la contraseña tiene menos de 6 caracteres
-    }
-  
-    if (password1 !== password2) {
-      showAlertError();
-      return false; // Evita que el formulario se envíe si las contraseñas no coinciden
-    }
-  
-    showAlertSuccess(); // Llama a la función en caso de éxito
-    return true; // Permite el envío del formulario si todo está correcto
-  }
   
